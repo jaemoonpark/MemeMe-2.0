9590d07b4fd3853cc4adc3b781cf6c662a21c61d
Jaemoon <jaemoon@jaemoonpark.com>
2016-11-15 13:56:07 -0500

    move meme update on tableviewcontroller to viewWillAppear function. make table reload data when viewWillAppear. corrected cellforRowAt function. TODO: make meme table cell prettier

--- MemeMe 2.0/MemeTableViewController.swift
+++ MemeMe 2.0/MemeTableViewController.swift
@@ -10,18 +10,32 @@ import Foundation
 import UIKit
 class MemeTableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
     var memes: [Meme]!
+    //adding outlet in order to call reloadData
+    @IBOutlet weak var tblMeme: UITableView!
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        //updating meme on viewWillAppear so meme properly updates when exiting meme editor
         memes = appDelegate.memes
+        tblMeme.reloadData()
     }
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "todo")!
-        return cell
-    }
+    
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return memes.count
     }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MemeTableCell")!
+        cell.imageView?.image = memes[indexPath.row].imageFinal
+        cell.textLabel?.text = memes[indexPath.row].strTop
+        cell.detailTextLabel?.text = memes[indexPath.row].strBtm
+        return cell
+    }
+    
 }
